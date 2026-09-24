c3541141dbe551bc016a1987a2a193ed5bfc828a
qmxcore-stack <qmxcore@gmail.com>
2026-09-24 23:45:25 +0800

    fix: update hero section image source

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero({ onSearch }: { onSearch?: () => void } = {}) {
     <section id="home" className="relative pt-24 sm:pt-28 pb-0 bg-white overflow-hidden">
       <div className="w-full">
         <img 
-          src="https://kitkecemasanhomeopathy.com/wp-content/uploads/2026/09/download-1.png" 
+          src="https://kitkecemasanhomeopathy.com/wp-content/uploads/2026/09/6da3978d-96d1-46f8-84b5-e5c9e7522dc5.png" 
           alt="DriveJer MPV Rental KL & Selangor" 
           className="w-full h-auto object-cover block"
         />
